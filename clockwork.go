600700c3b01a4e48ae63af63f14a3e80f693407f
George Vanburgh <george.vanburgh@student.manchester.ac.uk>
2016-03-23 15:52:15 +0000

    Add documentation. Refactor URL constants.

--- clockwork.go
+++ clockwork.go
@@ -8,51 +8,59 @@ import (
 )
 
 const (
-	smsURL        string = "https://api.clockworksms.com/xml/send.aspx"
-	creditURL     string = "https://api.clockworksms.com/xml/credit.aspx"
-	balanceURL    string = "https://api.clockworksms.com/xml/balance.aspx"
+	baseURL       string = "api.clockworksms.com"
+	smsSuffix     string = "/xml/send.aspx"
+	creditSuffix  string = "/xml/credit.aspx"
+	balanceSuffix string = "/xml/balance.aspx"
 	xmlTypeHeader string = "text/xml"
 )
 
+// Clockwork is the main class used to communicate with the Clockwork API
 type Clockwork struct {
 	Key string
 	SSL bool
 }
 
+// GetBalance returns number of SMS credits available to the
+// account associated with this Clockwork API key.
+// Deprecated: Use the GetBalance function to return a cash balance
 func (clockwork *Clockwork) GetBalance() *Balance {
 	request := balanceRequest{Key: clockwork.Key}
 	balanceToReturn := Balance{}
-	getXmlResponse(balanceURL, request, &balanceToReturn)
+	getXMLResponse(buildURL(balanceSuffix, clockwork.SSL), request, &balanceToReturn)
 	return &balanceToReturn
 }
 
+// GetCredit returns the current cash balance and currency information for
+// the account associated with this Clockwork API key.
 func (clockwork *Clockwork) GetCredit() *Credit {
 	request := creditRequest{Key: clockwork.Key}
 	creditToReturn := Credit{}
-	getXmlResponse(creditURL, request, &creditToReturn)
+	getXMLResponse(buildURL(creditSuffix, clockwork.SSL), request, &creditToReturn)
 	return &creditToReturn
 }
 
+// SendSMS sends a single SMS message
 func (clockwork *Clockwork) SendSMS(givenSMS SMS) *MessageResponse {
 	smsArray := []SMS{givenSMS}
 	return clockwork.SendMultipleSMS(smsArray)
 }
 
+// SendMultipleSMS sends an array of SMS messages
 func (clockwork *Clockwork) SendMultipleSMS(givenSMS []SMS) *MessageResponse {
 	request := MessageRequest{Key: clockwork.Key, SMS: givenSMS}
 	messageResponseToReturn := MessageResponse{}
-	getXmlResponse(smsURL, request, &messageResponseToReturn)
+	getXMLResponse(buildURL(smsSuffix, clockwork.SSL), request, &messageResponseToReturn)
 	return &messageResponseToReturn
 }
 
-// TODO: Should be a slightly more restricted interface?
-func getXmlResponse(url string, request interface{}, resultToFill interface{}) {
-	requestXml, marshalErr := xml.Marshal(request)
+func getXMLResponse(url string, request interface{}, resultToFill interface{}) {
+	requestXML, marshalErr := xml.Marshal(request)
 	if marshalErr != nil {
 		panic(marshalErr)
 	}
 
-	resp, postErr := http.Post(url, xmlTypeHeader, bytes.NewBuffer(requestXml))
+	resp, postErr := http.Post(url, xmlTypeHeader, bytes.NewBuffer(requestXML))
 	if postErr != nil {
 		panic(postErr)
 	}
@@ -69,6 +77,16 @@ func getXmlResponse(url string, request interface{}, resultToFill interface{}) {
 	}
 }
 
+func buildURL(suffix string, SSL bool) string {
+	if SSL {
+		return "https://" + baseURL + suffix
+	}
+
+	return "http://" + baseURL + suffix
+}
+
+// NewClockwork returns a new instance of the Clockwork client, using the given
+// API key
 func NewClockwork(Key string) *Clockwork {
 	return &Clockwork{Key: Key, SSL: true}
 }
